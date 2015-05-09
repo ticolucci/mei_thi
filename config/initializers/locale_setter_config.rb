dcc0df289963ac6dd335fc96cbf0077e1a918d54
Thiago Colucci <tcolucci@thoughtworks.com>
2015-05-09 23:45:47 +0800

    remove localhost from default locales

--- config/initializers/locale_setter_config.rb
+++ config/initializers/locale_setter_config.rb
@@ -1,7 +1,6 @@
 LocaleSetter.configure do |config|
   config.localized_domains = {
     "meiandthi.com" => :en,
-    "meiethi.com.br" => :pt,
-    "localhost" => :pt
+    "meiethi.com.br" => :pt
   }
 end
