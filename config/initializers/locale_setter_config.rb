11b5f620360e34b47753592fe24675ee9fdfb1a8
Thiago Colucci <tcolucci@thoughtworks.com>
2015-05-10 12:04:06 +0800

    try configuring everything in LocaleSetter

--- config/initializers/locale_setter_config.rb
+++ config/initializers/locale_setter_config.rb
@@ -1,6 +1,9 @@
 LocaleSetter.configure do |config|
+  config.url_param           = :locale
+  config.user_locale_method  = :locale
   config.localized_domains = {
     "www.meiandthi.com" => :en,
     "www.meiethi.com.br" => :pt
   }
+  config.current_user_method = :current_user
 end
